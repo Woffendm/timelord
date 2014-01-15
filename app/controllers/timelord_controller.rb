e5d3311620edbc85a28686ae3212d2f295434254
Woffendin, Michael Harrison <michael.woffendin@oregonstate.edu>
2014-01-15 15:30:51 -0800

    Get REST view working properly

--- app/controllers/timelord_controller.rb
+++ app/controllers/timelord_controller.rb
@@ -4,9 +4,14 @@
 class TimelordController < ApplicationController
   unloadable
  
+  accept_api_auth :index
+ 
   def index 
-    @result = Issue.get_average_issue_times(params).to_json
-    render "index", :layout => false
+    @result = Issue.get_average_issue_times(params)
+    respond_to do |format|
+      format.html { render "index", :layout => false}
+      format.api
+    end
   end
 
 
